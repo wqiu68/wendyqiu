d6cf243f2541ccb03fb8c902f63d0e621e8b35d7
wqiu68 <55062929+wqiu68@users.noreply.github.com>
2025-02-22 18:25:21 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+// Simple JavaScript for enabling smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+</script>
